6f011361462569bc4c90075b72ae23541059a0d9
clarachristiansen <s214659@dtu.dk>
2024-11-11 16:26:11 +0100

    message function almost done

--- src/models/painn.py
+++ src/models/painn.py
@@ -71,7 +71,7 @@ class PaiNN(nn.Module):
             contributions to the overall molecular property prediction.
         """
         s_i_0 = self.embedding_matrix(atoms)
-        v_i_0 = torch.zeros((atoms.shape[0], self.num_features, 3))        
+        v_i_0 = torch.zeros((atoms.shape[0], self.num_features, 3))  
 
         # Calculate vector between all nodes of same graph (molecule)
         r_ij= self.calculate_rij(atom_positions, graph_indexes, self.cutoff_dist)
@@ -81,10 +81,9 @@ class PaiNN(nn.Module):
 
         # Cap on distance
         raise NotImplementedError
-    
+    # REWRITE CALCULATE Rij to matrix format where i and j are row and column number and the element is the vector (three dimensions deep)
     def calculate_rij(self, atom_positions, graph_indexes, threshold):
-        unique_molecules = graph_indexes.unique()
-        vectors_per_molecule = [] #mol1: 0->1, 0->2, 0->3 ... 1->0, 1->2, 1->3 ...
+        #mol1: 0->1, 0->2, 0->3 ... 1->0, 1->2, 1->3 ...
         # what is missing is the index of the atoms and the adjacency matrix with 3 rows, index1, index2, vector rij.
         # Format:
         # Index i [0,0,0,0,..1,1,1,1,..2,2,2,2...]
@@ -99,7 +98,7 @@ class PaiNN(nn.Module):
 
         j = torch.arange(len(atom_positions))
         for i in range(len(atom_positions)):
-            molecule_mask = graph_indexes == graph_indexes[0]
+            molecule_mask = graph_indexes == graph_indexes[i]
             js = j[molecule_mask]
             molecule_positions = atom_positions[molecule_mask]
             vectors = atom_positions[i] - molecule_positions
@@ -114,20 +113,78 @@ class PaiNN(nn.Module):
         adjacency_matrix = torch.cat([index_i.unsqueeze(1), index_j.unsqueeze(1), r_ij], dim=1)
         return adjacency_matrix
     
+    #def calculate_rij_2(self, atom_positions, graph_indexes, threshold):
+
+        r_ij = torch.empty((len(atom_positions), len(atom_positions), 3))
+
+        j = torch.arange(len(atom_positions))
+        for i in range(len(atom_positions)):
+            molecule_mask = graph_indexes == graph_indexes[0]
+            js = j[molecule_mask]
+            molecule_positions = atom_positions[molecule_mask]
+            vectors = atom_positions[i] - molecule_positions
+
+            distance_mask = torch.linalg.norm(vectors, axis=1) < threshold
+            r_ij[i, js[distance_mask], :] = vectors[distance_mask]
+        return r_ij
+
+
     def RBF(self, r_ij):
-        norm_r_ij = torch.linalg.norm(r_ij)
+        vector_r_ij = r_ij[:,2:]
+        norm_r_ij = torch.linalg.norm(vector_r_ij, axis=1).unsqueeze(1) # normalize each r_ij vector not all into 1 number
         frac = torch.pi / self.cutoff_dist
-        rbf_result = torch.sin(torch.arange(1,21) * frac * norm_r_ij) / norm_r_ij
+        n = torch.arange(1,21).float().reshape(1, 20)
+        epsilon = 1e-8
+        rbf_result = torch.sin(n * frac * norm_r_ij) / (norm_r_ij + epsilon)
         return rbf_result
+    
+    def cosine_cutoff(self, r_ij): # OBS DONT KNOW IF IT IS CUTOFF_DISTANCE OR ANOTHER CUTOFF PARAMETER
+         # remember cosine cutoff: https://ml4chem.dev/_modules/ml4chem/atomistic/features/cutoff.html#Cosine has code
+        cutoff_array = 0.5 * (torch.cos(torch.pi * r_ij / self.cutoff_dist) + 1.0)
+        cutoff_array *= (r_ij < self.cutoff_dist).float()
+        return cutoff_array
+    
+
+        """cutoff_array = torch.where(
+        r_ij < self.cutoff_dist,
+        0.5 * (torch.cos(torch.pi * r_ij / self.cutoff_dist) + 1.0),
+        0.0)
+        return cutoff_array"""
         
     def message(self, v_j, s_j, r_ij):
+        n_features = 128
+        js=r_ij[:, 1].int()
+
         s_path = nn.Sequential(
             nn.Linear(128, 128, True),
             nn.SiLU(),
             nn.Linear(128, 384))
+        r_ij_path = nn.Sequential(
+            nn.Linear(20, 384, True))
+
+        ### FORWARD PASS IN MESSAGE:
+        phi = s_path(s_j)
         rbf_output = self.RBF(r_ij)
-        # remember cosine cutoff: https://ml4chem.dev/_modules/ml4chem/atomistic/features/cutoff.html#Cosine has code
+        W = self.cosine_cutoff(r_ij_path(rbf_output))
+        split = phi[js, :] * W # phi[j_index,:]
+        split_1 = split[:,:n_features]
+        split_2 = split[:,n_features:n_features*2]
+        split_3 = split[:,n_features*2:]
+
+        # Write it more compressed
+        left_1 = v_j[js,:,:] * split_1.unsqueeze(2) #v_j.shape torch.Size([1798, 128, 3]) and split_1.shape torch.Size([1798, 128]) 
+        # Should split_1 be multiplied on each x,y,z?
+        epsilon = 1e-8
+        right_1 = split_3.unsqueeze(1) * (r_ij[:,2:] / (torch.linalg.norm(r_ij[:,2:],axis=1).unsqueeze(1) + epsilon).unsqueeze(-1)).unsqueeze(-1)
+        left_2 = left_1 + right_1
+        left_2
+
+
+        pass
+
+       
         
         # Should this be made as an init and forward as well? Not necessary
 
         pass
+
